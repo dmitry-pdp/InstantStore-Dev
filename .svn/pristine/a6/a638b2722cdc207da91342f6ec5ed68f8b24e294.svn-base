792bbe4bec3d32803242ea98898a1247c2ee0542
dmitry-pdp <dmitry-pdp@users.noreply.github.com>
2015-02-25 22:29:22 -0800

    SVN removed

--- .svn/pristine/a6/a638b2722cdc207da91342f6ec5ed68f8b24e294.svn-base
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[ProductsCategories] (
-    [ProductId]         INT NOT NULL,
-    [ProductCategoryID] INT NOT NULL,
-    [SortOrder]         INT DEFAULT ((0)) NOT NULL,
-    CONSTRAINT [FK_category_productsCategories] FOREIGN KEY ([ProductCategoryID]) REFERENCES [dbo].[ProductCategories] ([ProductCategoryId]) ON DELETE CASCADE,
-    CONSTRAINT [FK_product_productsCategories] FOREIGN KEY ([ProductId]) REFERENCES [dbo].[Products] ([ProductId]) ON DELETE CASCADE
-);
-
---
+++ InstantStore.WebUI/Controllers/NavCategoryController.cs
@@ -0,0 +1,33 @@
+﻿using InstantStore.Domain.Abstract;
+using InstantStore.Domain.Entities;
+using InstantStore.WebUI.Utilities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace InstantStore.WebUI.Controllers
+{
+    //Navigation by Categories
+    public class NavCategoryController : Controller
+    {
+        IRepository repository;
+
+        public NavCategoryController(IRepository repo)
+        {
+            repository = repo;
+        }
+
+        public PartialViewResult Menu(string category = null)
+        {
+            ViewBag.SelectedCategory = category;
+
+            ProductCategory[] categories =
+                repository.Products
+                        .Select(x => x.Category)
+                        .DistinctBy(x => x.Name)
+                        .OrderBy(x => x.Name).ToArray();
+
+            return PartialView(categories);
+        }
+    }
+}
